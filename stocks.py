eeaa51e1efd21762c50031f2c5879b5788dc7f1f
unknown <houj@oxy.edu>
2020-02-19 18:09:55 -0800

    Return chart for APPL

--- stocks.py
+++ stocks.py
@@ -1,6 +1,25 @@
 from alpha_vantage.timeseries import TimeSeries
+from alpha_vantage.techindicators import TechIndicators
+from matplotlib.pyplot import figure
+import matplotlib.pyplot as plt
+
 # Your key here
 key = 'I8TTHGPZJU2EPWDJ'
-ts = TimeSeries(key)
-aapl, meta = ts.get_daily(symbol='AAPL')
-print(aapl['2020-02-19'])
+
+# Chose your output format, or default to JSON (python dict)
+ts = TimeSeries(key, output_format='pandas')
+ti = TechIndicators(key)
+
+# Get the data, returns a tuple
+# aapl_data is a pandas dataframe, aapl_meta_data is a dict
+aapl_data, aapl_meta_data = ts.get_daily(symbol='AAPL')
+# aapl_sma is a dict, aapl_meta_sma also a dict
+aapl_sma, aapl_meta_sma = ti.get_sma(symbol='AAPL')
+
+
+# Visualization
+figure(num=None, figsize=(15, 6), dpi=80, facecolor='w', edgecolor='k')
+aapl_data['4. close'].plot()
+plt.tight_layout()
+plt.grid()
+plt.show()
